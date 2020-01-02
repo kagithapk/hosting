e0cf8d026e36829e9d3eda7765fe522d468608dd
Praveen Kagitha <kagithapk@gmail.com>
2020-01-02 23:40:07 +0530

    Updates

---
+++ precache-manifest.428272f6593d65d689d2d5874f130e78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e310183ed3028c929a4dbf1faeabf72b",
+    "url": "/hosting/index.html"
+  },
+  {
+    "revision": "872c9ac901b1a105885d",
+    "url": "/hosting/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "32135abe8c71ac19ca35",
+    "url": "/hosting/static/js/2.189e469d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hosting/static/js/2.189e469d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "872c9ac901b1a105885d",
+    "url": "/hosting/static/js/main.d184e498.chunk.js"
+  },
+  {
+    "revision": "7f980caf8a75b4a8cca4",
+    "url": "/hosting/static/js/runtime-main.1aa5f847.js"
+  }
+]);
